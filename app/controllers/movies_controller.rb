ebb7b92236efc596c84f535ef12a5e54851cc934
useyourillusion <jarekb2413@gmail.com>
2017-07-01 07:01:22 -0700

    show,index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,37 @@
 class MoviesController < ApplicationController
+	before_action :find_movie, only: [:show,:edit,:update,:destroy]
 	def index
+		@movies = Movie.all.order("created_at DESC")
 	end
 
 	def new
 		@movie = Movie.new
 	end
 
+
+	def show
+		
+	end
+
+
 	def create
 		@movie = Movie.new(movie_params)
+		if @movie.save
+			redirect_to root_path
+		else 
+			render 'new'
+		end
 	end
 
 	private
 	def movie_params
 		params.require(:movie).permit(:title, :description, :director)
 	end
+
+	
+	def find_movie
+		@movie = Movie.find(params[:id])
+	end
+
+
 end
