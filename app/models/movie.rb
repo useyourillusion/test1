6d9e74685b6469d0958e65fbcf83d568838e5118
useyourillusion <jarekb2413@gmail.com>
2017-07-01 11:36:38 -0700

    associations,genres/nie wszystko

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+	belongs_to :user
+	belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701150217) do
+ActiveRecord::Schema.define(version: 20170701173937) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170701150217) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
