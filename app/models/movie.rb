05a25743722976e04df34703c187475058859757
useyourillusion <jarekb2413@gmail.com>
2017-07-02 11:47:43 -0700

    reviews contoller,model,reviews on show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
+	has_many :reviews
 
 	has_attached_file :movie_img, :styles=> { :movie_index=> "250x350>", :movie_show=> "325x100>" }, :default_url=> "/images/:style/missing.png"
   validates_attachment_content_type :movie_img, :content_type=> /\Aimage\/.*\z/
